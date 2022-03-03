bc3f90a62a5b9a12be9149466c85af5ff4f1f567
cyqg <cyqg@fresnou.org>
2022-03-03 08:55:22 -0800

    progress commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,16 @@
-const {User, Student} = require('../models');
+const {User, Student, Staff} = require('../models');
 const md5 = require('md5');
 const passport = require('passport');
-
 //render student registration form
 module.exports.renderStudentRegistrationForm = function(req, res){
-    res.render('user/register');
+    res.render('user/registerStudent');
 }
+
+//render staff registration form
+module.exports.renderStaffRegistrationForm = function(req, res){
+    res.render('user/registerStaff');
+}
+
 //register student account
 module.exports.registerStudent = async function(req, res){
     const user = await User.create({
@@ -22,9 +27,29 @@ module.exports.registerStudent = async function(req, res){
     res.redirect('/courses')
 }
 
+//register staff account
+module.exports.registerStaff = async function(req, res){
+    const user = await User.create({
+        email: req.body.email,
+        password:md5(req.body.password),
+        role: 'staff'
+    });
+    await Staff.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        user_id: user.id
+    });
+    res.redirect('/courses')
+}
+
+
 //Render the login form
 module.exports.renderLoginForm = function(req, res){
-    res.render('user/login')
+    let errorMessages = [];
+    if(req.session.messages){
+        errorMessages = req.session.messages;
+    }
+    res.render('user/login', {errorMessages})
 }
 
 //login user
@@ -34,4 +59,9 @@ module.exports.login = passport.authenticate('local', {
     failureMessage: true
 });
 
+//logout
+module.exports.logout = function(req, res){
+    req.logout();
+    res.redirect('/login');
+}
 
